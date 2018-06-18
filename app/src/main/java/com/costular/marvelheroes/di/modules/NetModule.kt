be6608d786bb91d2bba03acdd85414341dd6e66e
David Lopez Rodriguez <dloprodu@gmail.com>
2018-06-18 20:11:44 +0100

    Readme updated

--- app/src/main/java/com/costular/marvelheroes/di/modules/NetModule.kt
+++ app/src/main/java/com/costular/marvelheroes/di/modules/NetModule.kt
@@ -5,7 +5,7 @@ import com.costular.marvelheroes.data.net.MarvelHeroesService
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+// import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -22,9 +22,9 @@ class NetModule {
     fun provideRetrofit(): Retrofit {
         val client =
                 OkHttpClient.Builder()
-                        .addInterceptor(HttpLoggingInterceptor().apply {
-                            level = HttpLoggingInterceptor.Level.BODY
-                        })
+                       // .addInterceptor(HttpLoggingInterceptor().apply {
+                       //     level = HttpLoggingInterceptor.Level.BODY
+                       // })
                         .build()
 
         return Retrofit.Builder()
